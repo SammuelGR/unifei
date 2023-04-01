d7a2628eb126289d398a015f6f0bb3fc8c976a15
Sammuel Reis <sammuel.reis@ripio.com>
2023-03-31 23:44:35 -0300

    create exercicio2.c

---
+++ 2023_1/XDES01/Aula2/exercicio2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    printf("\tAs parelhas das carruagens do Farao\n");
+    printf("eu te comparo, minha amada.\n");
+    printf("\n");
+
+    printf("\tGraciosas sao tuas faces entre os brincos,\n");
+    printf("e teu pescoco entre colares.\n");
+    printf("\n");
+
+    printf("\tFaremos para ti brincos de ouro\n");
+    printf("com filigranas de prata.\n");
+
+    return 0;
+}
