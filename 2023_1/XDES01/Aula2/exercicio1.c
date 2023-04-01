89862f2c88463bad1f2bbbdb89f5103105d8afca
Sammuel Reis <sammuel.reis@ripio.com>
2023-03-31 23:39:39 -0300

    create exercicio1.c

---
+++ 2023_1/XDES01/Aula2/exercicio1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    printf("Por mim se vai das dores à morada,\n");
+    printf("Por mim se vai ao padecer eterno,\n");
+    printf("Por mim se vai à gente condenada.\n");
+    printf("\n");
+
+    printf("Moveu Justiça o Autor meu sempiterno,\n");
+    printf("Formado fui por divinal possança,\n");
+    printf("Sabedoria suma e amor supremo.\n");
+    printf("\n");
+
+    printf("No existir, ser nenhum a mim se avança,\n");
+    printf("Não sendo eterno, e eu eternal perduro:\n");
+    printf("Deixai, ó vós que entrais, toda a esperança!\n");
+    printf("\n");
+
+    printf("Estas palavras, em letreiro escuro,\n");
+    printf("Eu vi, por cima de uma porta escrito.\n");
+    printf("\"Seu sentido\" — disse eu — \"Mestre me é duro\"\n");
+
+    return 0;
+}
