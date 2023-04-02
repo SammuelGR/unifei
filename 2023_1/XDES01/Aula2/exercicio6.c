8f43c7e4e2e75cab561c7db479611aa10e72ed23
Sammuel Reis <sammuel.reis@ripio.com>
2023-04-02 18:52:16 -0300

    create exercicio6

---
+++ 2023_1/XDES01/Aula2/exercicio6.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    printf("SSSSSSSS\tRRRRRRRR\n");
+    printf("SS\t\tRR    RR\n");
+    printf("SSSSSSSS\tRRRRRR\n");
+    printf("      SS\tRR  RR\n");
+    printf("SSSSSSSS\tRR    RR\n");
+
+    return 0;
+}
