149809201e8da624339bb272ce947b573b6de683
Sammuel Reis <sammugr@hotmail.com>
2023-04-21 20:36:34 -0300

    create aula3 exercicio2

---
+++ 2023_1/XDES01/Aula3/exercicio2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    float userNumber = 0;
+
+    printf("Digite um numero real: ");
+    scanf("%f", &userNumber);
+
+    printf("Valor digitado: %f\n", userNumber);
+
+    return 0;
+}
