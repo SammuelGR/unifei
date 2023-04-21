0b839b2dd87184574347bfeff6ffd375c8c39598
Sammuel Reis <sammugr@hotmail.com>
2023-04-21 20:33:35 -0300

    create aula3 exercicio1

---
+++ 2023_1/XDES01/Aula3/exercicio1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main () {
+    int userNumber = 0;
+
+    printf("Digite um numero inteiro: ");
+    scanf("%d", &userNumber);
+    
+    printf("Valor digitado: %d\n", userNumber);
+
+    return 0;
+}
