e64234176405e1427248a902d6366d02a48ef04e
Sammuel Reis <sammuel.reis@ripio.com>
2024-10-17 21:17:00 -0300

    feat(stco01): add rainha files

--- 2024_2/STCO01/Rainha/main.c
+++ 2024_2/STCO01/Rainha/main.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void imprimir_tabuleiro(int r0, int r1, int r2) {
+void imprimir_tabuleiro(int r0, int r1, int r2, int r3) {
 	int i;
 
-	for (i = 0; i < 3; i++) {
+	for (i = 0; i < 4; i++) {
 		if (r0 == i) {
 			printf("R");
 		} else {
@@ -13,7 +13,7 @@ void imprimir_tabuleiro(int r0, int r1, int r2) {
 	}
 	printf("\n");
 
-	for (i = 0; i < 3; i++) {
+	for (i = 0; i < 4; i++) {
 		if (r1 == i) {
 			printf("R");
 		} else {
@@ -22,7 +22,7 @@ void imprimir_tabuleiro(int r0, int r1, int r2) {
 	}
 	printf("\n");
 
-	for (i = 0; i < 3; i++) {
+	for (i = 0; i < 4; i++) {
 		if (r2 == i) {
 			printf("R");
 		} else {
@@ -31,36 +31,47 @@ void imprimir_tabuleiro(int r0, int r1, int r2) {
 	}
 	printf("\n");
 
+	for (i = 0; i < 4; i++) {
+		if (r3 == i) {
+			printf("R");
+		} else {
+			printf("_");
+		}
+	}
+
+	printf("\n\n");
 	return;
 }
 
 int main(void) {
-	int r0, r1, r2;
+	int r0, r1, r2, r3;
 
 
-	for (r0 = 0; r0 < 3; r0++) {
-		for (r1 = 0; r1 < 3; r1++) {
-			for (r2 = 0; r2 < 3; r2++) {
-				// coluna
-				if (r0 == r1 || r0 == r2 || r1 == r2) {
-					continue;
+
+	for (r0 = 0; r0 < 4; r0++) {
+		for (r1 = 0; r1 < 4; r1++) {
+			for (r2 = 0; r2 < 4; r2++) {
+				for (r3 = 0; r3 < 4; r3++) {
+					// coluna vertical
+					if (r0 == r1 || r0 == r2 || r0 == r3 ||
+						r1 == r2 || r1 == r3 ||
+						r2 == r3
+						) {
+						continue;
+					}
+					// diagonal
+					if (r0 == r1 - 1 || r0 == r1 + 1 || r0 == r2 - 2 || r0 == r2 + 2 || r0 == r3 - 3 || r0 == r3 + 3 ||
+						r1 == r2 - 1 || r1 == r2 + 1 || r1 == r3 - 2 || r1 == r3 + 2 ||
+						r2 == r3 - 1 || r2 == r3 + 1
+						) {
+						continue;
+					}
+
+					imprimir_tabuleiro(r0, r1, r2, r3);
 				}
-
-				// diagonal
-				if (r0 == r1 - 1 || r0 == r1 + 1 ||
-					r0 == r2 - 2 || r0 == r2 + 2 ||
-					r1 == r2 - 1 || r1 == r2 + 1
-					) {
-					continue;
-				}
-
-				imprimir_tabuleiro(r0, r1, r2);
 			}
 		}
 	}
-
-
-
 
 	return 0;
 }
