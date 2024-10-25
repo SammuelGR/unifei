c34dd2b50808636cc02bf5f177feb5dc6a57545e
Sammuel Reis <sammuel.reis@ripio.com>
2024-10-25 20:54:33 -0300

    feat(stco01): add trab02 initial files

---
+++ 2024_2/STCO01/Trab02/n_queens_pilha.c
@@ -0,0 +1,13 @@
+/* submeta apenas esse arquivo, o pilha.c e pilha.h jah estao no servidor */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "pilha.h"
+
+int main(int argc, char* argv[]){
+    int n;
+    scanf("%d", &n);
+
+
+    return 0;
+}
