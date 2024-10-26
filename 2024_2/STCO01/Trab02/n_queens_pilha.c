683c75ec94d72d9b341dd054d8f64c72752ac15b
Sammuel Reis <sammuel.reis@ripio.com>
2024-10-25 22:12:14 -0300

    wip(stco01/trab02): wip

--- 2024_2/STCO01/Trab02/n_queens_pilha.c
+++ 2024_2/STCO01/Trab02/n_queens_pilha.c
@@ -2,12 +2,42 @@
 #include <stdlib.h>
 #include "pilha.h"
 
+void print_table(pilha p, int n) {
+	int i, j;
+
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < n; j++) {
+			if (desempilhar(p) == j)
+				printf("R ");
+			else printf("_ ");
+		}
+
+		printf("\n");
+	}
+}
+
+pilha solve(pilha p, int n, int current) {
+
+	if (n == current) {
+		print_table(p, n);
+		printf("\n");
+
+		return;
+	}
+
+	return p;
+}
+
 int main(void) {
+	pilha p = cria_pilha();
+
 	int n;
 
 	scanf("%d", &n);
 
-	printf("%d\n", n);
+	solve(p, n, 0);
+
+	libera_pilha(p);
 
 	return 0;
 }
