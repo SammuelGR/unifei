2e4f5f442520cf3f6578b2b1f1491a6ec07fd51e
Sammuel Reis <sammugr@hotmail.com>
2024-10-30 23:38:06 -0300

    feat(stco01/trab02): finish trab02
    dont stopppp, biliiiiviinnn

--- 2024_2/STCO01/Trab02/n_queens_pilha.c
+++ 2024_2/STCO01/Trab02/n_queens_pilha.c
@@ -2,11 +2,23 @@
 #include <stdlib.h>
 #include "pilha.h"
 
-void print_table(pilha p, int n) {
-	int i, j = n;
+void printTable(pilha p, int n) {
+	int i;
+	pilha reversedStack = cria_pilha();
+	pilha tempStack = cria_pilha();
 
-	while (j--) {
+	while (!pilha_vazia(p)) {
 		int c = desempilhar(p);
+		empilhar(tempStack, c);
+		empilhar(reversedStack, c);
+	}
+
+	while (!pilha_vazia(tempStack)) {
+		empilhar(p, desempilhar(tempStack));
+	}
+
+	while (!pilha_vazia(reversedStack)) {
+		int c = desempilhar(reversedStack);
 
 		for (i = 0; i < n; i++)
 			if (c == i)
@@ -18,31 +30,56 @@ void print_table(pilha p, int n) {
 	}
 
 	printf("\n");
+
+	libera_pilha(reversedStack);
+	libera_pilha(tempStack);
 }
 
-void solveStack(pilha p, int n) {
-	int i, j;
+int canKill(int *currentBoard, int currentHeight, int x) {
+	for (int j = 0; j < currentHeight; j++) {
+		int dist = currentHeight - j;
 
-	pilha copy = cria_pilha();
+		if (currentBoard[j] == x || currentBoard[j] - x == dist || x - currentBoard[j] == dist) {
+			return 1;
+		}
+	}
+	return 0;
+}
 
-	for (i = 0; i < n; i++) {
-		for (j = 0; j < n; j++) {
-			int c = pilha_vazia(p) ? 0 : desempilhar(p);
-			empilhar(copy, c);
+void solve(pilha p, int n) {
+	int *currentBoard = (int *)malloc(n * sizeof(int));
+	int height = 0;
+	int curPosition = 0;
 
-			int canKill = c == j && !pilha_vazia(p);
+	while (height >= 0) {
+		if (curPosition >= n) {
+			height--;
 
-			if (!canKill) {
-				while (!pilha_vazia(copy)) empilhar(p, desempilhar(copy));
-
-				empilhar(p, c);
-				empilhar(p, j);
-				break;
+			if (height >= 0 && !pilha_vazia(p)) {
+				curPosition = desempilhar(p) + 1;
 			}
+		} else if (!canKill(currentBoard, height, curPosition)) {
+			currentBoard[height] = curPosition;
+
+			empilhar(p, curPosition);
+
+			height++;
+			if (height == n) {
+				printTable(p, n);
+
+				height--;
+				if (!pilha_vazia(p)) {
+					curPosition = desempilhar(p) + 1;
+				}
+			} else {
+				curPosition = 0;
+			}
+		} else {
+			curPosition++;
 		}
 	}
 
-	print_table(p, n);
+	free(currentBoard);
 }
 
 int main(void) {
@@ -52,7 +89,7 @@ int main(void) {
 
 	scanf("%d", &n);
 
-	solveStack(p, n);
+	solve(p, n);
 
 	libera_pilha(p);
 
