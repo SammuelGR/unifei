8b95528e24bcecb97d67ca8a01af9821d5ad08bc
Sammuel Reis <sammugr@hotmail.com>
2024-11-12 21:18:18 -0300

    feat(xdes02): trab10 (aula16)

--- 2024_2/XDES02/Aula16/aula16-ex1.py
+++ 2024_2/XDES02/Aula16/aula16-ex1.py
@@ -1,10 +1,12 @@
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import messagebox, simpledialog
+from typing import List
 
 class ModelCliente():
-	def __init__(self, nome, email):
+	def __init__(self, nome, email, codigo):
 		self.__nome = nome
 		self.__email = email
+		self.__codigo = codigo
 
 	@property
 	def nome(self):
@@ -14,6 +16,10 @@ class ModelCliente():
 	def email(self):
 		return self.__email
 
+	@property
+	def codigo(self):
+		return self.__codigo
+
 class View():
 	def __init__(self, master, controller):
 		self.controller = controller
@@ -21,18 +27,24 @@ class View():
 		self.janela.pack()
 		self.frame1 = tk.Frame(self.janela)
 		self.frame2 = tk.Frame(self.janela)
+		self.frame3 = tk.Frame(self.janela)
 		self.frame1.pack()
 		self.frame2.pack()
+		self.frame3.pack()
 
 		self.labelInfo1 = tk.Label(self.frame1,text="Nome: ")
 		self.labelInfo2 = tk.Label(self.frame2,text="Email: ")
+		self.labelInfo3 = tk.Label(self.frame3,text="Código: ")
 		self.labelInfo1.pack(side="left")
 		self.labelInfo2.pack(side="left")
+		self.labelInfo3.pack(side="left")
 
 		self.inputText1 = tk.Entry(self.frame1, width=20)
 		self.inputText1.pack(side="left")
 		self.inputText2 = tk.Entry(self.frame2, width=20)
 		self.inputText2.pack(side="left")
+		self.inputText3 = tk.Entry(self.frame3, width=20)
+		self.inputText3.pack(side="left")
 
 		self.buttonSubmit = tk.Button(self.janela,text="Salva")
 		self.buttonSubmit.pack(side="left")
@@ -46,27 +58,32 @@ class View():
 		self.buttonList.pack(side="left")
 		self.buttonList.bind("<Button>", controller.listHandler)
 
+		self.buttonSearch = tk.Button(self.janela,text="Pesquisar")
+		self.buttonSearch.pack(side="left")
+		self.buttonSearch.bind("<Button>", controller.searchHandler)
+
 	def mostraJanela(self, titulo, mensagem):
 		messagebox.showinfo(titulo, mensagem)
+
+	def mostraDialog(self, titulo, mensagem):
+		return simpledialog.askstring(titulo, mensagem, parent=self.janela)
 
 class Controller():
 	def __init__(self):
 		self.root = tk.Tk()
 		self.root.geometry('300x100')
-		self.listaClientes = []
+		self.listaClientes: List[ModelCliente] = []
 
-		# Cria a view passando referência da janela principal e
-		# de si próprio (controlador)
 		self.view = View(self.root, self)
 
 		self.root.title("Exemplo MVC")
-		# Inicia o mainloop
 		self.root.mainloop()
 
 	def salvaHandler(self, event):
 		nomeCli = self.view.inputText1.get()
 		emailCli = self.view.inputText2.get()
-		cliente = ModelCliente(nomeCli, emailCli)
+		codCli = self.view.inputText3.get()
+		cliente = ModelCliente(nomeCli, emailCli, codCli)
 		self.listaClientes.append(cliente)
 		self.view.mostraJanela('Sucesso', 'Cliente cadastrado com sucesso')
 		self.clearHandler(event)
@@ -74,13 +91,35 @@ class Controller():
 	def clearHandler(self, event):
 		self.view.inputText1.delete(0, len(self.view.inputText1.get()))
 		self.view.inputText2.delete(0, len(self.view.inputText2.get()))
+		self.view.inputText3.delete(0, len(self.view.inputText3.get()))
 
 	def listHandler(self, event):
-		clientsStr = 'Nome  --  Email\n'
+		clientsStr = 'Nome  --  Email  --  Código\n'
 		for cliente in self.listaClientes:
-			clientsStr += cliente.nome + '  --  ' + cliente.email + '\n'
+			clientsStr += \
+				cliente.nome + \
+				'  --  ' + \
+				cliente.email + \
+				'  --  ' + \
+				cliente.codigo + \
+				'\n'
 
 		self.view.mostraJanela('Clientes cadastrados', clientsStr)
+
+	def searchHandler(self, event):
+		searchStr = self.view.mostraDialog("Pesquisa", "Código: ")
+		clienteBusca = None
+
+		for cliente in self.listaClientes:
+			if cliente.codigo == searchStr:
+				clienteBusca = cliente
+
+		if clienteBusca != None:
+			self.view.mostraJanela("Cliente encontrado", \
+						  f'Nome: {cliente.nome} \nEmail: {cliente.email}')
+		else:
+			self.view.mostraJanela("Não enconrtrado", "Código não cadastrado")
+
 
 if __name__ == '__main__':
 	c = Controller()
