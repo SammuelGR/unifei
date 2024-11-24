659309667d8c57be918a073a2e6c200a5a3f235a
Sammuel Reis <sammugr@hotmail.com>
2024-11-24 20:40:18 -0300

    feat(xdes02/trab12): artista.py

--- 2024_2/XDES02/Trabalho12/artista.py
+++ 2024_2/XDES02/Trabalho12/artista.py
@@ -1,3 +1,6 @@
+import tkinter as tk
+from tkinter import messagebox
+
 from typing import List
 
 from album import Album
@@ -38,3 +41,75 @@ class Artista:
 
 	def addMusica(self, musica: Musica):
 		self.musicas.append(musica)
+
+class CtrlArtista:
+	def __init__(self) -> None:
+		self.__listaArtistas: List[Artista] = []
+
+	@property
+	def listaArtistas(self):
+		return self.__listaArtistas
+
+	def insereArtista(self):
+		self.limiteIns = LimiteInsereArtista(self)
+
+	def mostraArtistas(self):
+		text = 'Nome\n'
+
+		for artista in self.listaArtistas:
+			text += artista.nome
+
+		self.limiteLista = LimiteMostraArtistas(text)
+
+	def enterHandler(self, event):
+		nome = self.limiteIns.inputNome.get()
+
+		artista = Artista(nome)
+		self.listaArtistas.append(artista)
+
+		self.limiteIns.mostraJanela('Sucesso', 'Artista cadastrado com sucesso')
+		self.clearHandler(event)
+
+	def clearHandler(self, event):
+		self.limiteIns.inputNome.delete(0, len(self.limiteIns.inputNome.get()))
+
+	def fechaHandler(self, event):
+		self.limiteIns.destroy()
+
+
+class LimiteInsereArtista(tk.Toplevel):
+	def __init__(self, controle: CtrlArtista):
+		tk.Toplevel.__init__(self)
+		self.geometry('250x100')
+		self.title('Artista')
+		self.controle = controle
+
+		self.frameNome = tk.Frame(self)
+		self.frameButton = tk.Frame(self)
+		self.frameNome.pack()
+		self.frameButton.pack()
+
+		self.labelNome = tk.Label(self.frameNome, text='Nome: ')
+		self.labelNome.pack(side='left')
+
+		self.inputNome = tk.Entry(self.frameNome, width=20)
+		self.inputNome.pack(side='left')
+
+		self.buttonSubmit = tk.Button(self.frameButton, text='Enter')
+		self.buttonSubmit.pack(side='left')
+		self.buttonSubmit.bind('<Button>', controle.enterHandler)
+
+		self.buttonClear = tk.Button(self.frameButton, text='Clear')
+		self.buttonClear.pack(side='left')
+		self.buttonClear.bind('<Button>', controle.clearHandler)
+
+		self.buttonFecha = tk.Button(self.frameButton, text='Concluído')
+		self.buttonFecha.pack(side='left')
+		self.buttonFecha.bind('<Button>', self.controle.fechaHandler)
+
+	def mostraJanela(self, titulo: str, msg: str):
+		messagebox.showinfo(titulo, msg)
+
+class LimiteMostraArtistas:
+	def __init__(self, text: str) -> None:
+		messagebox.showinfo('Lista de artistas', text)
---
+++ 2024_2/XDES02/Trabalho12/main.py
@@ -0,0 +1,75 @@
+import tkinter as tk
+
+from artista import CtrlArtista
+
+class ControlePrincipal():
+	def __init__(self):
+		self.root = tk.Tk()
+
+		self.ctrlArtista = CtrlArtista()
+
+		self.limite = LimitePrincipal(self.root, self)
+
+		self.root.title('Player de música MVC')
+		self.root.mainloop()
+
+	def insereArtista(self):
+		self.ctrlArtista.insereArtista()
+
+	def consultaArtista(self):
+		self.ctrlArtista.mostraArtistas()
+
+	def insereAlbum():
+		pass
+
+	def consultaAlbum():
+		pass
+
+	def inserePlaylist():
+		pass
+
+	def consultaPlaylist():
+		pass
+
+	def sair(self):
+		self.root.destroy()
+
+class LimitePrincipal():
+	def __init__(self, root: tk.Tk, controle: ControlePrincipal):
+		self.controle = controle
+
+		self.root = root
+		self.root.geometry('300x300')
+
+		self.menubar = tk.Menu(self.root)
+		self.artistaMenu = tk.Menu(self.menubar)
+		self.albumMenu = tk.Menu(self.menubar)
+		self.playlistMenu = tk.Menu(self.menubar)
+		self.sairMenu = tk.Menu(self.menubar)
+
+		self.artistaMenu.add_command(label='Cadastrar', \
+							   command=self.controle.insereArtista)
+		self.artistaMenu.add_command(label='Consultar', \
+							   command=self.controle.consultaArtista)
+		self.menubar.add_cascade(label='Artista', \
+						   menu=self.artistaMenu)
+
+		self.albumMenu.add_command(label='Cadastrar', \
+							 command=self.controle.insereAlbum)
+		self.albumMenu.add_command(label='Consultar', \
+							 command=self.controle.consultaAlbum)
+		self.menubar.add_cascade(label='Album', menu=self.albumMenu)
+
+		self.playlistMenu.add_command(label='Cadastrar', \
+								command=self.controle.inserePlaylist)
+		self.playlistMenu.add_command(label='Consultar', \
+								command=self.controle.consultaPlaylist)
+		self.menubar.add_cascade(label='Playlist', menu=self.playlistMenu)
+
+		self.sairMenu.add_command(label='Sair', command=self.controle.sair)
+		self.menubar.add_cascade(label='Sair', menu=self.sairMenu)
+
+		self.root.config(menu=self.menubar)
+
+if __name__ == '__main__':
+	c = ControlePrincipal()
