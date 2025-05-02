c52a6354ab320f935b6c11ba52d97b72d2f2a6e9
Sammuel Reis <sammugr@hotmail.com>
2025-05-02 20:23:29 -0300

    feat(stco02): trab01 add values to hash table

--- 2025_01/STCO02/Trab01/main.py
+++ 2025_01/STCO02/Trab01/main.py
@@ -1,29 +1,48 @@
-from typing import Optional, Tuple
+from typing import List, Optional, Tuple
 
 class Node:
-	def __init__(self, key: Optional[str] = None):
+	def __init__(self, key: Optional[str] = None, values: Optional[List[Tuple[str]]] = None):
 		self.key: Optional[str] = key
+		self.values: Optional[List[Tuple[str]]] = values
 		self.leftNode: Optional[Node] = None
 		self.rightNode: Optional[Node] = None
+	
+	def find(self, key: str):
+		if self.key is None:
+			return None
 
-	def insert(self, key: str):
+		if key == self.key:
+			return self.values
+		
+		if key < self.key:
+			if self.leftNode is None:
+				return None
+			return self.leftNode.find(key)
+		
+		if self.rightNode is None:
+			return None
+		
+		return self.rightNode.find(key)
+
+	def insert(self, key: str, values: List[Tuple[str]]):
 		if not self.key:
 			self.key = key
+			self.values = values
 			return
 
 		if key < self.key:
 			if self.leftNode:
-				self.leftNode.insert(key)
+				self.leftNode.insert(key, values)
 				return
 
-			self.leftNode = Node(key)
+			self.leftNode = Node(key, values)
 			return
 
 		if self.rightNode:
-			self.rightNode.insert(key)
+			self.rightNode.insert(key, values)
 			return
 
-		self.rightNode = Node(key)
+		self.rightNode = Node(key, values)
 	
 	def preorder(self):
 		return (self.key, \
@@ -45,11 +64,16 @@ class HashTable:
 
 		return hashValue % self.size
 	
-	def put(self, key: str):
+	def put(self, key: str, values: List[Tuple[str]]):
 		hashValue = self.getHash(key)
 
-		self.slots[hashValue].insert(key)
+		self.slots[hashValue].insert(key, values)
+	
+	def get(self, key: str):
+		hashValue = self.getHash(key)
 
+		return self.slots[hashValue].find(key)
+		
 	def listAll(self):
 		for slot in self.slots:
 			if slot.key is not None:
@@ -57,9 +81,18 @@ class HashTable:
 
 
 table = HashTable()
-table.put('Máquina de maionese')
-table.put('Elixir da vida')
-table.put('Sementes Silvestres (Ver)')
-table.put('Piso Rústico de Tábuas')
+table.put('Máquina de maionese', [('Madeira', '15'), ('Pedra', '15'), ('Cristal de terra', '1'), ('Barra de cobre', '1')])
+table.put('Elixir da vida', [('Cogumelo vermelho', '1'), ('Cogumelo roxo', '1'), ('Morel', '1'), ('Cantarelo', '1')])
+table.put('Sementes Silvestres (Ver)', [('Café de jardim', '1')])
+table.put('Piso Rústico de Tábuas', [('Madeira', '1')])
+table.put('Jarra de conserva', [('Madeira', '50'), ('Pedra', '40'), ('Carvão', '8')])
 
-table.listAll()
+# table.listAll()
+
+print(table.get('Máquina de maionese'))
+print(table.get('Elixir da vida'))
+print(table.get('Sementes Silvestres (Ver)'))
+print(table.get('Piso Rústico de Tábuas'))
+print(table.get('Jarra de conserva'))
+print(table.get('AAAAAAAAAAAAAA'))
+
