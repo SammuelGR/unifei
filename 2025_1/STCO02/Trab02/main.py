6192a9beb491c3849d4f6735d55ea4bc872b4e4e
Sammuel Reis <sammugr@hotmail.com>
2025-05-09 19:30:02 -0300

    wip(stco02): wip trab02

--- 2025_1/STCO02/Trab02/main.py
+++ 2025_1/STCO02/Trab02/main.py
@@ -13,6 +13,9 @@ def getCreatures(fileName: str):
 			ratings = []
 			for i in range(1, 7):
 				ratings.append(float(line[i]))
+			
+			ratings.append(0) # sum of ratings
+			ratings.append(0) # sum of points
 
 			creatures[name] = ratings
 	return creatures
@@ -22,8 +25,6 @@ if __name__ == '__main__':
 	usersQty = int(input())
 	ratings = []
 
-	creatures = getCreatures(fileName)
-
 	for _ in range(0, usersQty):
 		currentRatings = input().strip().split(' ')
 
@@ -32,4 +33,18 @@ if __name__ == '__main__':
 		
 		ratings.append(currentRatings)
 
-	print(creatures)
+	creatures = getCreatures(fileName)
+
+	for userRating in ratings:
+		for creature, cRatings in creatures.items():
+			total = 0
+			
+			for i in range(0, 6):
+				total += cRatings[i] * userRating[i]
+			
+			creatures[creature][6] = total
+
+		
+
+		for creature, cRatings in creatures.items():
+			pass
