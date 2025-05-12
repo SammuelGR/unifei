34436b97196bb0c67a1becf571afb06195223469
Sammuel Reis <sammugr@hotmail.com>
2025-05-11 23:55:09 -0300

    feat(stco02): trab02 sort creatures by single user rating

--- 2025_1/STCO02/Trab02/main.py
+++ 2025_1/STCO02/Trab02/main.py
@@ -14,13 +14,14 @@ def getCreatures(fileName: str):
 			for i in range(1, 7):
 				ratings.append(float(line[i]))
 
-
 			creatures.append((name, *ratings))
 	return creatures
 
 if __name__ == '__main__':
 	fileName = input()
 	usersQty = int(input())
+
+	creatures = getCreatures(fileName)
 
 	ratings = []
 	for _ in range(0, usersQty):
@@ -31,17 +32,21 @@ if __name__ == '__main__':
 
 		ratings.append(currentRatings)
 
-	creatures = getCreatures(fileName)
-
-	for userRating in ratings[1:2]: # TODO: remove limit
+	for userRating in ratings[2:3]: # TODO: remove limit
 		for i in range(0, len(creatures)):
 			total = 0
 			for j in range(0, 6):
 				total += userRating[j] * creatures[i][j + 1]
 
-			newCreature = (*creatures[i], total)
-			creatures[i] = newCreature
+			updatedCreature = (*creatures[i][0:7], total)
+			creatures[i] = updatedCreature
 
+		for i in range(0, len(creatures) - 1):
+			for j in range(1, len(creatures)):
+				if creatures[i][7] < creatures[j][7]:
+					creatures[i], creatures[j] = creatures[j], creatures[i]
+				elif creatures[i][7] == creatures[j][7]:
+					if creatures[i][0] > creatures[j][0]:
+						creatures[i], creatures[j] = creatures[j], creatures[i]
 
 	print(creatures)
-
