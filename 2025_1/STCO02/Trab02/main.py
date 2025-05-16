65d30ade567c22bd5d7068ad614252dc67845a5d
Sammuel Reis <sammugr@hotmail.com>
2025-05-16 19:39:41 -0300

    feat(stco02): trab02 use linear sorting

--- 2025_1/STCO02/Trab02/main.py
+++ 2025_1/STCO02/Trab02/main.py
@@ -17,11 +17,68 @@ def getCreatures(fileName: str):
 			creatures.append((name, *ratings))
 	return creatures
 
+
+def sortByName(creatures):
+	maxLen = 0
+
+	for i in range(len(creatures)):
+		nameLen = len(creatures[i][0])
+		if nameLen > maxLen:
+			maxLen = nameLen
+
+	for pos in range(maxLen - 1, -1, -1):
+		buckets = [[] for _ in range(256)] 
+
+		for i in range(len(creatures)):
+			name = creatures[i][0]
+			code = ord(name[pos]) if pos < len(name) else 0
+			buckets[code].append(creatures[i])
+
+		idx = 0
+		for b in buckets:
+			for c in b:
+				creatures[idx] = c
+				idx += 1
+
+	return creatures
+
+def parsePoints(points: float):
+	return int(points * 1000)
+
+def sortByUserRating(creatures, creaturesSize, scoreIdx):
+	minScore = maxScore = parsePoints(creatures[0][scoreIdx])
+	for i in range(1, creaturesSize):
+		currScore = parsePoints(creatures[i][scoreIdx])
+		if currScore < minScore:
+			minScore = currScore
+		if currScore > maxScore:
+			maxScore = currScore
+
+	rangeSize = maxScore - minScore + 1
+	C = [0 for _ in range(rangeSize)]
+
+	for i in range(creaturesSize):
+		idx = maxScore - parsePoints(creatures[i][scoreIdx])
+		C[idx] += 1
+
+	for i in range(1, rangeSize):
+		C[i] += C[i - 1]
+
+	sortedCreatures = [None for _ in range(creaturesSize)]
+
+	for i in range(creaturesSize - 1, -1, -1):
+		idx = maxScore - parsePoints(creatures[i][scoreIdx])
+		sortedCreatures[C[idx] - 1] = creatures[i]
+		C[idx] -= 1
+
+	return sortedCreatures
+
+
 if __name__ == '__main__':
 	fileName = input()
 	usersQty = int(input())
 
-	creatures = getCreatures(fileName)
+	creatures = sortByName(getCreatures(fileName))
 	creaturesSize = len(creatures)
 
 	ratings = []
@@ -42,22 +99,13 @@ if __name__ == '__main__':
 			prevPoints = creatures[i][8] if len(creatures[i]) >= 9 else 0
 			creatures[i] = (*creatures[i][0:7], total, prevPoints)
 
-		for i in range(0, creaturesSize - 1):
-			for j in range(i + 1, creaturesSize):
-				if creatures[i][7] < creatures[j][7]:
-					creatures[i], creatures[j] = creatures[j], creatures[i]
-				elif creatures[i][7] == creatures[j][7]:
-					if creatures[i][0] > creatures[j][0]:
-						creatures[i], creatures[j] = creatures[j], creatures[i]
+		creatures = sortByUserRating(creatures, creaturesSize, 7)
 
 		for i in range(0, creaturesSize):
 			prevPoints = creatures[i][8]
 			creatures[i] = (*creatures[i][0:8], prevPoints + (creaturesSize - i) - 1)
 
-	for i in range(0, creaturesSize - 1):
-		for j in range(i + 1, creaturesSize):
-			if creatures[i][8] < creatures[j][8]:
-				creatures[i], creatures[j] = creatures[j], creatures[i]
+	creatures = sortByUserRating(creatures, creaturesSize, 8)
 
 	for creature in creatures:
 		print(f'{creature[0]} {creature[8]}')
