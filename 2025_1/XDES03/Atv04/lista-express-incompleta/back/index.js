44b10c6a98c5081c264d8641c70db4eda2c9314e
Sammuel Reis <sammugr@hotmail.com>
2025-05-25 22:44:07 -0300

    feat(XDES03): complete Atv04

--- 2025_1/XDES03/Atv04/lista-express-incompleta/back/index.js
+++ 2025_1/XDES03/Atv04/lista-express-incompleta/back/index.js
@@ -1,24 +1,91 @@
-
-const express = require('express');
+const express = require("express");
 const app = express();
-const fs = require('fs');
-const cors = require('cors');
-const path = require('path');
+const fs = require("fs");
+const cors = require("cors");
+const path = require("path");
 
-//Necessário para extrair os dados de Forms vindos de uma requisição POST
 app.use(express.json());
 app.use(cors());
 
-
 app.listen(3000, () => {
-    console.log('Servidor na porta 3000');
+  console.log("Servidor na porta 3000");
 });
 
+const usersDbPath = path.join("db", "banco-dados-usuario.json");
+const coursesDbPath = path.join("db", "cursos.json");
 
-//Requisicao com POST publica para autenticar usuário...
-app.post('/login', async (req,res) => {
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
 
-    
-})
+  const users = JSON.parse(fs.readFileSync(usersDbPath, { encoding: "utf-8" }));
 
-//...criar as demais rotas seguindo os requisitos.
+  const emailExists = users.find((user) => user.email === email);
+
+  if (!emailExists) {
+    res
+      .status(404)
+      .send(
+        `Usuario com email ${email} não existe. Considere criar uma conta!.`
+      );
+
+    return;
+  }
+
+  if (emailExists.password !== password) {
+    res.status(400).send("Usuario ou senhas incorretas.");
+
+    return;
+  }
+
+  res.status(200).send("Autenticado com Sucesso");
+});
+
+app.post("/create", (req, res) => {
+  const { username, email, password } = req.body;
+
+  const users = JSON.parse(fs.readFileSync(usersDbPath, { encoding: "utf-8" }));
+  const emailExists = users.find((user) => user.email === email);
+
+  if (emailExists) {
+    res.status(400).send(`Usuario com email ${email} já existe.`);
+
+    return;
+  }
+
+  const newUser = { id: users.length + 1, username, email, password };
+  users.push(newUser);
+
+  fs.writeFileSync(usersDbPath, JSON.stringify(users, null, 2), {
+    encoding: "utf-8",
+  });
+
+  res.status(201).send("Tudo certo usuario criado com sucesso.");
+});
+
+app.get("/cursos", (_, res) => {
+  const courses = JSON.parse(
+    fs.readFileSync(coursesDbPath, { encoding: "utf-8" })
+  );
+
+  return res.json(courses);
+});
+
+app.get("/cursos/:nome", (req, res) => {
+  const { nome } = req.params;
+
+  const courses = JSON.parse(
+    fs.readFileSync(coursesDbPath, { encoding: "utf-8" })
+  );
+
+  const filteredCourses = courses.filter((course) =>
+    course.nome.toLocaleLowerCase().includes(nome.toLocaleLowerCase())
+  );
+
+  if (filteredCourses.length === 0) {
+    res.status(404).send(`Curso ${nome} Não Encontrado!`);
+
+    return;
+  }
+
+  res.json(filteredCourses);
+});
--- 2025_1/XDES03/Atv04/lista-express-incompleta/front/src/login/Login.jsx
+++ 2025_1/XDES03/Atv04/lista-express-incompleta/front/src/login/Login.jsx
@@ -1,53 +1,50 @@
-import '../styles/CreateUser.css';
-import {useForm} from 'react-hook-form';
-import axios, * as others from 'axios';
-import { useState } from 'react';
-import { Link, Navigate } from 'react-router-dom';
+import "../styles/CreateUser.css";
+import { useForm } from "react-hook-form";
+import axios from "axios";
+import { useState } from "react";
+import { Link, Navigate } from "react-router-dom";
 
+export default function Login() {
+  const [msg, setMsg] = useState(" ");
 
-export default function Login(){
+  const form = useForm();
 
-    const [msg, setMsg] = useState(' ');
+  const { register, handleSubmit } = form;
 
-    const form = useForm();
-
-    const { register, handleSubmit, formState } = form;
-
-    const {errors} = formState;
-
-    const submit = async (data) => {
-        
-        try {
-            const response = await axios.post('http://localhost:3000/login', data);
-            setMsg(response.data);
-        } catch (error) {
-            setMsg(error.response.data);
-        }   
-        
+  const submit = async (data) => {
+    try {
+      const response = await axios.post("http://localhost:3000/login", data);
+      setMsg(response.data);
+    } catch (error) {
+      setMsg(error.response.data);
     }
+  };
 
-    if(msg.includes('Autenticado')){
-        return <Navigate to='/cursos' />
-    }
+  if (msg.includes("Autenticado")) {
+    return <Navigate to="/cursos" />;
+  }
 
-    return (
-        <>  
-            <h2>Entre para acessar os serviços</h2>
-            <form onSubmit={handleSubmit(submit)} noValidate>
+  return (
+    <>
+      <h2>Entre para acessar os serviços</h2>
 
-                <label htmlFor="email" placeholder="email">Email</label>
-                <input type="text" id="email" {...register('email')} />
-                
-                <label htmlFor="password">Senha</label>
-                <input type="password" id="password" {...register('password')} />
-                
-                <button>Entrar</button>
-            </form>
-            <p className="server-response">{msg}</p>
-            <div className="realizar-cadastro">
-                Não possui conta? 
-                <Link to="/criar-user">Cadastro</Link>
-            </div>
-        </>
-    )
+      <form onSubmit={handleSubmit(submit)} noValidate>
+        <label htmlFor="email" placeholder="email">
+          Email
+        </label>
+
+        <input type="text" id="email" {...register("email")} />
+
+        <label htmlFor="password">Senha</label>
+        <input type="password" id="password" {...register("password")} />
+
+        <button>Entrar</button>
+      </form>
+      <p className="server-response">{msg}</p>
+      <div className="realizar-cadastro">
+        Não possui conta?
+        <Link to="/criar-user">Cadastro</Link>
+      </div>
+    </>
+  );
 }
