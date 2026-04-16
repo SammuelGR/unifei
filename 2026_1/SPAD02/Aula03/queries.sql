3160f8926b168f745f9ac32d55a24534d7f5d9a2
Sammuel Reis <sammugr@hotmail.com>
2026-04-15 23:52:30 -0300

    feat(spad02): complete aula03

--- 2026_1/SPAD02/Aula03/queries.sql
+++ 2026_1/SPAD02/Aula03/queries.sql
@@ -35,8 +35,8 @@ GRANT SELECT ON TABLE northwind.products, northwind.categories, northwind.suppli
 -- products
 GRANT INSERT, UPDATE ON TABLE northwind.products TO role_programadores
 
--- • Bloquear o acesso a tabela employees // ✅
--- • Proibir o DELETE na tabela products. // ✅
+-- • Bloquear o acesso a tabela employees ✅
+-- • Proibir o DELETE na tabela products. ✅
 
 -- Questão 4: Concedendo privilégios ao usuário.
 -- a) Considere que o usuário que você criou na questão 2 é um usuário do tipo
@@ -52,7 +52,71 @@ UPDATE northwind.products SET unitsinstock = 31 WHERE productid = 77 -- ✅
 
 -- c) Simule falhas associadas a falta de privilégios por esse usuário.
 -- d) Qual comportamento as consultas abaixo geram?
--- a. UPDATE northwind.products SET unitprice = 20 WHERE productid = 1;
--- b. DELETE FROM northwind.products WHERE productid = 1;
--- c. CREATE TABLE northwind.temp_teste_produtos ( id serial PRIMARY KEY,
--- nome_teste varchar(50) );
+    -- a. UPDATE northwind.products SET unitprice = 20 WHERE productid = 1; ✅
+    -- b. DELETE FROM northwind.products WHERE productid = 1; ❌
+    -- c. CREATE TABLE northwind.temp_teste_produtos ( id serial PRIMARY KEY,
+    -- nome_teste varchar(50) ); ❌
+
+-- Questão 5: Criação de Role Relatórios
+-- A diretoria da Northwind contratou um analista de dados. Ele precisa extrair indicadores
+-- de desempenho, mas jamais pode alterar um único dado ou ver informações privadas
+-- de funcionários.
+
+CREATE ROLE role_relatorios
+-- Especificações da Role: role_relatorios
+-- • Permitir que a role se conecte ao banco northwind
+GRANT CONNECT ON DATABASE northwind TO role_relatorios
+
+-- • Permitir que a role acesse o esquema northwind
+GRANT USAGE ON SCHEMA northwind TO role_relatorios
+
+-- • O analista deve conseguir ler todas as tabelas do schema
+GRANT SELECT ON ALL TABLES IN SCHEMA northwind TO role_relatorios
+REVOKE SELECT ON TABLE northwind.employees FROM role_relatorios
+
+-- • A role não pode ter permissão de INSERT, UPDATE ou DELETE em nenhuma
+-- hipótese. ✅
+
+-- • Criar o usuário analista_bi e vinculá-lo a esta role.
+CREATE USER analista_bi WITH PASSWORD '123'
+GRANT role_relatorios TO analista_bi
+
+-- Questão 6: Atualização de Schema
+-- a. Volte no perfil DBA (usuário postgres) e crie a tabela abaixo
+CREATE TABLE northwind.promocoes
+( id serial PRIMARY KEY, nome_promo varchar(50) );
+
+-- b. Logue no banco como o usuário analista_bi ✅
+-- c. Qual comportamento as consultas abaixo geram?
+    -- a. Selecionar os 10 primeiros registros da tabela produtos
+    SELECT * FROM northwind.products LIMIT 10 -- ✅
+
+    -- b. Selecionar todos os registros da tabela northwind.promocoes
+    SELECT * FROM northwind.promocoes -- ❌
+    -- permission denied pois a tabela foi criada após o grant
+
+-- Questão 7: Avaliando privilégios do usuário.
+select * from information_schema.role_table_grants where grantee= 'role_relatorios'; -- a)
+select * from information_schema.role_table_grants where grantee= 'analista_bi'; -- b)
+SELECT * FROM pg_roles -- c)
+-- • Anotar o id de analista_bi e role_relatorios
+-- 16677, 16676
+
+SELECT * FROM pg_auth_members -- d)
+GRANT DELETE ON northwind.products TO analista_bi -- e)
+select * from information_schema.role_table_grants where grantee= 'analista_bi'; -- f)
+
+SELECT r.rolname as usuario, g.rolname as grupo
+    FROM pg_auth_members m
+    JOIN pg_roles r ON m.member = r.oid
+    JOIN pg_roles g ON m.roleid = g.oid
+    WHERE r.rolname = 'analista_bi';
+
+-- Questão 8: Removendo privilégios do usuário.
+-- a. Remova da role 'programadores' o privilégio de inserir novos produtos no banco
+REVOKE INSERT ON TABLE northwind.products FROM role_programadores
+
+-- b. Teste a remoção do privilégio.
+INSERT INTO northwind.products
+	VALUES (666,'crack', 666, 666, 'bastante', 10, 4, 1, 1, 0)
+-- ❌ permission denied ✅
